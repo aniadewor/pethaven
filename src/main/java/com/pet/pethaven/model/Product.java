cb9fd8f48dcaa37b282224123cb828a267100c0b
aniadewor <116655947+aniadewor@users.noreply.github.com>
2026-03-11 17:10:42 +0100

    add validation for Product

--- src/main/java/com/pet/pethaven/model/Product.java
+++ src/main/java/com/pet/pethaven/model/Product.java
@@ -1,6 +1,7 @@
 package com.pet.pethaven.model;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,13 +22,19 @@ import java.util.List;
 public class Product {
     @Id
     private String id;
+    @NotNull(message = "Name cannot be null")
     private String name;
+    @NotNull(message = "Brand cannot be null")
     private String brand;
     @Min(value = 1)
+    @NotNull(message = "Price cannot be null")
     private Integer price;
+    @NotNull(message = "Category cannot be null")
     private String category;
     @Min(value = 1)
+    @NotNull(message = "stockQuantity cannot be null")
     private Integer stockQuantity;
+    @NotNull(message = "targetAnimal cannot be null")
     private TargetAnimal targetAnimal;
     List<String> tags = new ArrayList<>();
 }
