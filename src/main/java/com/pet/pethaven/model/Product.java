123df3aa53e0c05c4e563870dfbf2cff435c34c3
aniadewor <116655947+aniadewor@users.noreply.github.com>
2026-03-16 16:06:27 +0100

    add description

--- src/main/java/com/pet/pethaven/model/Product.java
+++ src/main/java/com/pet/pethaven/model/Product.java
@@ -22,6 +22,7 @@ public class Product {
     @Id
     private String id;
     private String name;
+    private String description;
     private String brand;
     @Min(value = 1)
     private Integer price;
